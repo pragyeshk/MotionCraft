1b0f6ee0e684fab902bc9eac630a966e672b3469
Pragyesh <pragyesh.singh@woliba.io>
2025-02-08 10:27:03 +0530

    Initial commit

---
+++ MotionCraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotionCraft
+//
+//  Created by Pragyesh Kumar Singh on 08/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotionCraft/MotionCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionCraftApp.swift
+//  MotionCraft
+//
+//  Created by Pragyesh Kumar Singh on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MotionCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
